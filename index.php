118e71679a37693a4fd3138714e854d58c753b8f
PhanNgocHoang <52957470+PhanNgocHoang@users.noreply.github.com>
2019-08-23 21:54:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <style>
+        h1 {
+            text-align: center;
+            font-size: 450%;
+            color: white;
+            margin-top: 10%;
+            margin-right: 40%;
+        }
+        
+        body {
+            background-image: url("atn2.jpg");
+            
+        }
+        .login{
+            margin-top:20%;
+        }
+        .serior{
+            margin-left:5%;
+            margin-right:10%;
+        }
+    </style>
+    <title>ATN</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Company ATN</h1>
+        <div class="login">
+            <a href="login1.php"><button class="btn btn-primary serior">Login For Serior Staff</button></a>
+            <a href="login2.php"><button class="btn btn-primary">Login For Store</button></a>
+        </div>
+        
+        
+    </div>
+</body>
+</html>
+
