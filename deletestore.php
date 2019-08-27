6fca936e0ce03242cdbdcb42330976cfda416ba5
PhanNgocHoang <52957470+PhanNgocHoang@users.noreply.github.com>
2019-08-27 13:50:42 +0700

    Update deletestore.php

--- deletestore.php
+++ deletestore.php
@@ -8,5 +8,5 @@ $query = $pdo->prepare($sql);
 $query->execute(array(':id' => $id));
  
 //redirecting to the display page (index.php in our case)
-header("Location:Store.php");
+header("Location:managestore.php");
 ?>
